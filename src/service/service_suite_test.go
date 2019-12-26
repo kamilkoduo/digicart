0a658b04d77bb39721069af30c6787099b67fa5f
kamilkoduo <kamilkoduo@gmail.com>
2019-12-26 13:51:18 +0300

    + tests boilerplate

---
+++ src/service/service_suite_test.go
@@ -0,0 +1,14 @@
+//+build unit
+package service_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestService(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Service Suite")
+}
---
+++ src/service/service_test.go
@@ -0,0 +1,13 @@
+//+build unit
+package service_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "service"
+)
+
+var _ = Describe("Service", func() {
+
+})
