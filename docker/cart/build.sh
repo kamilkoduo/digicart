5e49d22e3ac736083122b04a04c86f2e3d147984
Kamil Akhmetov <kamilkoduo@gmail.com>
2019-12-27 18:37:57 +0300

    fixes (#6)
    * fixed repo local tag
    * ! fixes

--- docker/cart/build.sh
+++ docker/cart/build.sh
@@ -4,7 +4,7 @@ build(){
 local DOCKER_DIR=docker
 local COMPONENT=cart
 local TAG=local
-local DOCKER_REGISTRY=registry.github.com/kamilkoduo/digicart/${COMPONENT}
+local DOCKER_REGISTRY=registry.local/kamilkoduo/digicart/${COMPONENT}
 local DOCKER_PATH=${DOCKER_DIR}/${COMPONENT}/Dockerfile
 local CONTEXT=.
 docker build -t ${DOCKER_REGISTRY}:${TAG} -f ${DOCKER_PATH} ${CONTEXT} --no-cache
