0f64639889249552a7bd5651b21144449d3c162e
kamilkoduo <kamilkoduo@gmail.com>
2019-12-27 14:41:04 +0300

    + 1 test works

--- src/service/service_test.go
+++ src/service/service_test.go
@@ -37,42 +37,66 @@ var _ = Describe("Cart Service", func() {
 	})
 
 	Describe("Unit Testing", func() {
-		Context("cart exists with one item", func() {
-			Context("check cart exists", func() {
-				It("should be correct", func() {
-					//test data
-					const (
-						cartID     = "1"
-						cartType   = api.CartTypeAuthorized
-						cartItemID = "101"
-						offerPrice = float64(4000)
-						count      = uint(1)
-						offerID    = "10101"
-					)
-					var offerTitle = map[string]interface{}{"ru": "Элемент корзины 101"}
-					//expectations on DB server mock
-					dbs.EXPECT().CartIDIsPresent(cartID).Return(true).Times(1)
-					dbs.EXPECT().GetCartType(cartID).Return(uint8(cartType)).Times(1)
-					dbs.EXPECT().GetMergedCartIDs(cartID).Return([]string{}).Times(1)
-					dbs.EXPECT().GetCartItemIDs(cartID).Return([]string{cartItemID}).Times(1)
-					dbs.EXPECT().CartItemIDIsPresent(cartID, cartItemID).Return(true).Times(1)
-					dbs.EXPECT().GetCartItemInfo(cartID, cartItemID).Return(map[string]string{
-						keys.ItemOfferIDMapKey():    offerID,
-						keys.ItemCountMapKey():      strconv.FormatUint(uint64(count), 10),
-						keys.ItemOfferPriceMapKey(): fmt.Sprintf("%f", offerPrice),
-					}).Times(1)
-					dbs.EXPECT().GetCartItemOfferTitle(cartID, cartItemID).Return(offerTitle)
+		Describe("Cart API methods", func() {
+			Describe("Get Cart", func() {
+				Context("Cart exists", func() {
+					It("returns cart", func() {
+						//test data
+						const (
+							cartID     = "1"
+							cartType   = api.CartTypeAuthorized
+							cartItemID = "101"
+							offerPrice = float64(4000)
+							count      = uint(1)
+							offerID    = "10101"
+						)
+						var offerTitle = map[string]interface{}{"ru": "Элемент корзины 101"}
+						//expectations on DB server mock
+						dbs.EXPECT().CartIDIsPresent(cartID).Return(true).Times(1)
+						dbs.EXPECT().GetCartType(cartID).Return(uint8(cartType)).Times(1)
+						dbs.EXPECT().GetMergedCartIDs(cartID).Return([]string{}).Times(1)
+						dbs.EXPECT().GetCartItemIDs(cartID).Return([]string{cartItemID}).Times(1)
+						dbs.EXPECT().CartItemIDIsPresent(cartID, cartItemID).Return(true).Times(1)
+						dbs.EXPECT().GetCartItemInfo(cartID, cartItemID).Return(map[string]string{
+							keys.ItemOfferIDMapKey():    offerID,
+							keys.ItemCountMapKey():      strconv.FormatUint(uint64(count), 10),
+							keys.ItemOfferPriceMapKey(): fmt.Sprintf("%f", offerPrice),
+						}).Times(1)
+						dbs.EXPECT().GetCartItemOfferTitle(cartID, cartItemID).Return(offerTitle)
 
-					cart, err := s.GetCart(cartID)
-					Expect(err).ShouldNot(HaveOccurred())
-					Expect(len(cart.Items)).Should(Equal(1))
-					Expect(cart.CartType).Should(Equal(api.CartTypeAuthorized))
-					Expect(cart.Items[0].CartItemID).Should(Equal(cartItemID))
-					Expect(cart.Items[0].OfferPrice).Should(Equal(offerPrice))
-					Expect(cart.Items[0].Count).Should(Equal(count))
-					Expect(cart.Items[0].OfferTitle).Should(Equal(offerTitle))
-					Expect(cart.Items[0].OfferID).Should(Equal(offerID))
+						cart, err := s.GetCart(cartID)
+						Expect(err).ShouldNot(HaveOccurred())
+						Expect(len(cart.Items)).Should(Equal(1))
+						Expect(cart.CartType).Should(Equal(cartType))
+						Expect(cart.Items[0].CartItemID).Should(Equal(cartItemID))
+						Expect(cart.Items[0].OfferPrice).Should(Equal(offerPrice))
+						Expect(cart.Items[0].Count).Should(Equal(count))
+						Expect(cart.Items[0].OfferTitle).Should(Equal(offerTitle))
+						Expect(cart.Items[0].OfferID).Should(Equal(offerID))
+					})
+				})
+			})
+			Describe("Get Cart Type", func() {
+				Context("Cart exists", func() {
+					It("returns cart type", func() {
+						//test data
+						const (
+							cartID   = "1"
+							cartType = api.CartTypeAuthorized
+						)
+						//expectations on DB server mock
+						dbs.EXPECT().CartIDIsPresent(cartID).Return(true).Times(1)
+						dbs.EXPECT().GetCartType(cartID).Return(uint8(cartType)).Times(1)
 
+						cartTypeR, err := s.GetCartType(cartID)
+						Expect(err).ShouldNot(HaveOccurred())
+						Expect(cartTypeR).Should(Equal(cartType))
+					})
+				})
+				Context("Cart does not exist", func() {
+					It("replies Not Found", func() {
+
+					})
 				})
 			})
 		})
